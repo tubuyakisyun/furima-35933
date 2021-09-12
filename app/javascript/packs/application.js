83cb844f70819c27c35e784fb3d98233d0ff0f7d
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-12 18:27:12 +0900

    デフォルトの記述を削除（turbolinksが読み込まれないように編集）
    最終課題では、JavaScriptを使って実装する箇所があります。実装の際に、注意すべき点が1点あります。
    
    それは、ページにアクセスしても、JavaScriptが作動しない場合があるということです。その多くは標準で導入されている、turbolinksというGemが原因で起こります。
    
    turbolinksによって画面遷移は高速化しますが、次ページのHTML要素だけを読み取って表示するため、JavaScriptファイルが読み込まれず、JavaScriptにて設定したイベントが発火しないケースがあります。
    
    turbolinksは、アプリケーションを作成すると標準で有効になっています。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()  ⇦この行を削除する　これで、turbolinksが無効化できました
 require("@rails/activestorage").start()
 require("channels")
 
