8e368dd2d5b9b2fad36cdc077f49b98f137d847f
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-17 18:59:07 +0900

    ログイン画面に遷移するように実装し,ユーザーの名前をDBへ保存できるように設定
    ログイン画面に遷移するように実装し,ユーザーの名前をDBへ保存できるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:show, :index] 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+
+
+  private
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+                                      :sign_up, 
+                                      keys: [
+                                             :nickname,
+                                             :email,
+                                             :password,
+                                             :birth_date,
+                                             :last_name,
+                                             :first_name,
+                                             :last_name_kana,
+                                             :first_name_kana
+                                             ])
+  end
 end
