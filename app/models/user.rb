ef2b1d32b3967f291cd2e4c258c1f5ad9fbe0e2e
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-17 14:45:30 +0900

    user バリデーションの記述を行う

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,      presence: true       
+  validates :birth_date,      presence: true
+  validates :last_name,      presence: true 
+  validates :first_name,      presence: true       
+  validates :last_name_kana,      presence: true       
+  validates :first_name_kana,      presence: true    
+  
+  has_many :items,          foreign_key: :user_id,dependent: :destroy
+  has_many :purchases,      foreign_key: :user_id,dependent: :destroy
+
+
 end
