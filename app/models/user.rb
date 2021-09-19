e2ca4050eea3d90b9c85cebf7599a245a525057f
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-19 15:02:48 +0900

    お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須であること。 お名前カナ(全角)は、全角（カタカナ）での入力が必須であること。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :birth_date, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana"}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana"}
 
   has_many :items,          foreign_key: :user_id, dependent: :destroy
   has_many :purchases,      foreign_key: :user_id, dependent: :destroy
