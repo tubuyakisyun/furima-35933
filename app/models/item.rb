75532823ef7e6107ae562b19de2e715694505651
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-23 22:56:45 +0900

    item バリデーション設定　アソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :sales_status
+  belongs_to :scheduled_delivery
+  belongs_to :shipping_fee_status
+
+  validates :name, :info, :price, presence: true
+
+  validates :category_id,            numericality: { other_than: 1 } 
+  validates :prefecture_id,          numericality: { other_than: 1 } 
+  validates :sales_status_id,        numericality: { other_than: 1 } 
+  validates :scheduled_delivery_id,  numericality: { other_than: 1 } 
+  validates :shipping_fee_status_id, numericality: { other_than: 1 } 
+
 end
