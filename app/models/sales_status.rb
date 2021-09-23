b1cd0929237d6be3d00594f492c88b5c702a0e1e
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-23 21:46:58 +0900

    sales_status モデルを編集

---
+++ app/models/sales_status.rb
@@ -0,0 +1,11 @@
+class sales_status < ActiveHash::Base
+  self.date = [
+    { id: 1, date: '--' },
+    { id: 2, date: '新品' },
+    { id: 3, date: '未使用に近い' },
+    { id: 4, date: '目たった傷や汚れなし' },
+    { id: 5, date: 'やや傷や汚れあり' },
+    { id: 6, date: '傷や汚れあり' },
+    { id: 7, date: '全体的に状態が悪い' }
+  ]
+end
