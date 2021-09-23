ba38af20ae48ceae2b059f6f428f7aeedf7f23dd
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-24 01:32:22 +0900

    validates にimageを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to :scheduled_delivery
   belongs_to :shipping_fee_status
 
-  validates :name, :info, :price, presence: true
+  validates :name, :info, :price,:image, presence: true
 
   validates :category_id,            numericality: { other_than: 1 , message: "can't be blank"} 
   validates :prefecture_id,          numericality: { other_than: 1 , message: "can't be blank"} 
