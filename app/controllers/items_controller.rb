97c0f965ea48e61e27e9750e8b1420ac0b45b298
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-25 13:37:38 +0900

    NameError uninitialized constant による　モデルクラス名を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,37 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @items = Item.all 
   end
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit
+    (
+      :image,
+      :name,
+      :info,
+      :price,
+      :category_id,
+      :prefecture_id
+      :salesstatus_id,
+      :scheduleddelivery_id,
+      :shippingfeestatus_id
+    )
+  end
+
 end
