3f7f1167472be2cf772d6b14676f89e04bd626c6
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-25 13:37:28 +0900

    NameError uninitialized constant による　モデルクラス名を修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,16 @@
-class category < ActiveHash::Base
-  self.date = [
-    { id: 1, date: '---' },
-    { id: 2, date: 'レディース' },
-    { id: 3, date: 'メンズ' },
-    { id: 4, date: 'ベビー・キッズ' },
-    { id: 5, date: 'インテリア・住まい・小物' },
-    { id: 6, date: '本・音楽・ゲーム' },
-    { id: 7, date: 'おもちゃ・ホビー・グッズ' },
-    { id: 8, date: '家電・スマホ・カメラ' },
-    { id: 9, date: 'スポーツ・レジャー' },
-    { id: 10, date: 'ハンドメイド' },
-    { id: 11, date: 'その他' }
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, category_id: '---' },
+    { id: 2, category_id: 'レディース' },
+    { id: 3, category_id: 'メンズ' },
+    { id: 4, category_id: 'ベビー・キッズ' },
+    { id: 5, category_id: 'インテリア・住まい・小物' },
+    { id: 6, category_id: '本・音楽・ゲーム' },
+    { id: 7, category_id: 'おもちゃ・ホビー・グッズ' },
+    { id: 8, category_id: '家電・スマホ・カメラ' },
+    { id: 9, category_id: 'スポーツ・レジャー' },
+    { id: 10, category_id: 'ハンドメイド' },
+    { id: 11, category_id: 'その他' }
   ]
   include ActiveHash::Associations
   has_many :items
