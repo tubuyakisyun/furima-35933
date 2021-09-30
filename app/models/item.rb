5745602bfe707cf8f805fc647545b1f714c506f7
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-30 16:37:36 +0900

    商品出品機能のテストコードを書く

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,29 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
-
   belongs_to :category
   belongs_to :prefecture
-  belongs_to :sales
+  belongs_to :status  
   belongs_to :scheduled
   belongs_to :shipping
 
-  validates :name, :info, :price,:image, presence: true
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :info
+    validates :price, 
+    numericality: { with: /\A[0-9]+\z/, message: "Half-width number" }
+    validates :price,
+    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }
+  end
 
-  validates :category_id,          numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :prefecture_id,        numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :sales_id,             numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :scheduled_id,         numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :shipping_id,          numericality: { other_than: 1 , message: "can't be blank"} 
-  
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :prefecture_id
+    validates :status_id
+    validates :scheduled_id
+    validates :shipping_id
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,121 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品できる場合' do
+      it '必要な情報を適切に入力して「出品する」ボタンを押すと、商品情報がデータベースに保存されること' do
+        expect(@item).to be_valid
+      end
+      it '商品名が必須であること' do
+        @item.name = 'aaa'
+        expect(@item).to be_valid
+      end
+      it '商品の説明が必須であること' do
+        @item.info = 'abba'
+        expect(@item).to be_valid
+      end
+      it 'カテゴリーの情報が必須であること' do
+        @item.category_id = '2'
+        expect(@item).to be_valid
+      end
+      it '商品の状態の情報が必須であること' do
+        @item.status_id = '2'
+        expect(@item).to be_valid
+      end
+      it '配送料の負担の情報が必須であること' do
+        @item.shipping_id = '2'
+        expect(@item).to be_valid
+      end
+      it '発送元の地域の情報が必須であること' do
+        @item.prefecture_id = '2'
+        expect(@item).to be_valid
+      end
+      it '発送までの日数の情報が必須であること' do
+        @item.scheduled_id = '2'
+        expect(@item).to be_valid
+      end
+      it '価格の情報が必須であること' do
+        @item.price = '7000'
+        expect(@item).to be_valid
+      end
+      it '価格は、¥300~¥9,999,999の間のみ保存可能であること' do
+        @item.price = '99999'
+        expect(@item).to be_valid
+      end
+      it '価格は、¥300で保存可能であること' do
+        @item.price = '300'
+        expect(@item).to be_valid
+      end
+      it '価格は、¥9,999,999で保存可能であること' do
+        @item.price = '9999999'
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品できない場合' do
+      it '商品名が空だと出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Name can't be blank"}
+      end
+      it '商品の説明が空だと出品できない' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Info can't be blank"}
+      end
+      it '価格が空だと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Price can't be blank"}
+      end
+      it '価格は、¥300以下だと保存できないこと' do
+        @item.price = '222'
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Price Out of setting range"}
+      end
+      it '価格は、¥9,999,999以上だと保存できないこと' do
+        @item.price = '22222222222'
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Price Out of setting range"}
+      end
+      it '価格は半角数値ではないと保存できないこと' do
+        @item.price = '９９９９９'
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Price Half-width number"}
+      end
+      it '写真が付かないと出品できない' do
+        @item.image.key = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Image can't be blank"}
+      end
+      it 'カテゴリーが空だと出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Category can't be blank"}
+      end
+      it '商品の状態の情報が空だと出品できない' do
+        @item.status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Sales can't be blank"}
+      end
+      it '配送料の負担の情報が空だと出品できない' do
+        @item.shipping_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Shipping can't be blank"}
+      end
+      it '発送元の地域の情報が空だと出品できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Prefecture can't be blank"}
+      end
+      it '発送までの日数の情報が空だと出品できない' do
+        @item.scheduled_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include{"Scheduled can't be blank"}
+      end
+    end
+  end
 end
