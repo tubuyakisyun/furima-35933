6efedc190f9c5c0821871241da8bc07c67489621
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-05 00:28:32 +0900

    コントローラーファイルにedit,updateアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
