3a65f82bb0b8549b7bfa73346cb7e3bb7293b7b4
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-05 20:49:32 +0900

    ルーティングにdestroyを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :destroy]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -32,6 +32,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    item.destroy
   end
 
   private
