fd3edef42d61d1bfb4d48fb41b77ea242657b366
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-08 03:48:31 +0900

    address モデルを生成
    バリデーションを設定
    マイグレーションファリルにカラムを記述

---
+++ app/models/address.rb
@@ -0,0 +1,16 @@
+class Address < ApplicationRecord
+  belongs_to :purchase
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_number
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+end
+
