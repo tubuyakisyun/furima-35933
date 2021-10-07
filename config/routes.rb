687364c60ea9fd021cc88df61decbfc9326346cb
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-08 04:22:01 +0900

    購入商品内容を表示する時、どの商品を購入したかを判断できるようにルーティングのネストを設定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :items, only: [:new,:create,:index,:show,:edit,:update,:destroy]
+  resources :items, only: [:new,:create,:index,:show,:edit,:update,:destroy] do
+    resources :purchases, only: [:index,:create]
+  end
+
   root to: 'items#index'
-  resources :purchases, only: [:index,:create]
 end
