309da9302683eb0ca49a4107c8c1429f20170e12
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-08 04:12:08 +0900

    purchase情報が保存できるように記述してみる
    purchase ビューファイルを記述
    purchases　コントローラをある程度記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,21 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  def index
+    @purchase.name = @item.id.name
+  end
+  
+  def create
+    @purchase = Purchase.create(purchase_params)
+    Address.create(address_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def purchase_params
+    params.merge(user_id: current_user.id, item_id:@item.id)
+  end
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :phone_number, :building_name).merge(purchase_id: @purchase.id)  
+  end
 end
