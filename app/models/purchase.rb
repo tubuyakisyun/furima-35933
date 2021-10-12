fc28ea48a81d3338c47e2029900b9c55e7a43482
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-13 00:26:51 +0900

    購入（purchase）モデルのアソシエーションを訂正

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,8 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :item
-  has_one :address
+  has_one :address,foreign_key: :purchase_id, dependent: :destroy
+
   validates :user_id, presence: true
   validates :item_id, presence: true
 end
