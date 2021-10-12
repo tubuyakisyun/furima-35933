ccce2a63ee2fe3d625c64eb01ad59f7c8bddcb87
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-13 00:33:35 +0900

    購入内容表示、 配送先の入力 できるようにpurchaseコントローラを記述する

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,21 +1,27 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
-    @purchase.name = @item.id.name
+    @item = Item.find(params[:item_id])
+  end
+
+  def new
   end
   
   def create
-    @purchase = Purchase.create(purchase_params)
-    Address.create(address_params)
+    item = Item.find(params[:item_id])
+    @purchase = item.create_purchase!(user_id: current_user.id)
+    @purchase.create_address!(address_params)
     redirect_to root_path
   end
 
   private
 
-  def purchase_params
-    params.merge(user_id: current_user.id, item_id:@item.id)
-  end
+  # def purchase_params 
+  #   params.merge(user_id: current_user.id, item_id: params[:item_id])
+  # end 
+
   def address_params
-    params.permit(:postal_code, :prefecture_id, :city, :address, :phone_number, :building_name).merge(purchase_id: @purchase.id)  
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :phone_number, :building_name).merge(purchase_id: @purchase.id)  
   end
+  
 end
