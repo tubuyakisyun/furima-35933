0214a0f0d602555c767d703dac9bf45012429e64
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-17 22:59:08 +0900

    purchase_controllerにて＠purchase_addressを生成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,6 +2,7 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
     @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
   end
 
   def new
@@ -9,12 +10,13 @@ class PurchasesController < ApplicationController
   end
   
   def create
+    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
       @purchase_address.save
       redirect_to root_path
     else
-      render new
+      render :index
     end
   #   item = Item.find(params[:item_id])
   #   @purchase = item.create_purchase!(user_id: current_user.id)
@@ -31,7 +33,8 @@ class PurchasesController < ApplicationController
   # def address_params
     # params.permit(:postal_code, :prefecture_id, :city, :house_number, :phone_number, :building_name).merge(purchase_id: @purchase.id)  
   # end
+
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :phone_number, :building_name).merge(user_id: current_user.id, purchase_id: @purchase.id)
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :phone_number, :building_name).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
