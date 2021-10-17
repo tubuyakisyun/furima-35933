e16618497e91728c708cd9a3c9147f73b08ded18
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-17 22:56:09 +0900

    バリデーションを訂正し、重複するエラーメッセージを表示しないよう

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,26 +1,20 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :city, :house_number, :phone_number, :prefecture_id
-  
+  attr_accessor :user_id, :item_id, :postal_code, :city, :house_number, :phone_number, :building_name, :prefecture_id
+
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)", allow_blank: true }
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :house_number
-    validates :phone_number
+    validates :phone_number, format: { with: /\A0[0-9]+\z/, message: 'number is invalid. Include half-width numbers' , allow_blank: true } 
     validates :user_id
     validates :item_id
   end
 
-  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
-
   def save
-    purchase = Purchase.create(user_id: user_id,item_id: item_id)
-    Address.create(postal_code: postal_code, city: city, house_number: house_number, phone_number: phone_number], prefecture_id: prefecture_id, purchase_id: purchase.id)
-
-    # item = Item.find(params[:item_id])
-    # @purchase = item.create_purchase!(user_id: current_user.id)
-    # @purchase.create_address!(address_params)
-    # redirect_to root_path
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, city: city, house_number: house_number, phone_number: phone_number, building_name: building_name, prefecture_id: prefecture_id, purchase_id: purchase.id)
   end
-  
+
 end
