6a89f7e3ac75307ae940448aaf4d2d1f9389e71e
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-20 20:20:35 +0900

    「クレジットカードの情報を削除」の修正

--- app/javascript/channels/card.js
+++ app/javascript/channels/card.js
@@ -22,10 +22,10 @@ const pay = () => {
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      document.getElementById("purchase_address_number").removeAttribute("name");
-      document.getElementById("purchase_address_cvc").removeAttribute("name");
-      document.getElementById("purchase_address_exp_month").removeAttribute("name");
-      document.getElementById("purchase_address_exp_year").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
