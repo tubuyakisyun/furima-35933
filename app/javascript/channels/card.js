599ea8076c347459339cf6c9396b19beed03ef30
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-20 18:52:30 +0900

    トークン化の処理
    1. クライアントサイドでPAY.JPのAPIを使用するために必要なJavaScriptを読み込む
    2. 実際にトークン化を行うJavaScriptファイルを作成する
    3. PAY.JPのテスト公開鍵を取得し設定する
    4. トークン化の処理を行う

---
+++ app/javascript/channels/card.js
@@ -0,0 +1,30 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_a07bf33444c4b839eeb331ad"); // PAY.JPテスト公開鍵
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchase_address[number]"),
+      cvc: formData.get("purchase_address[cvc]"),
+      exp_month: formData.get("purchase_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_address[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        debugger;
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
