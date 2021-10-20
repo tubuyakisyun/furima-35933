903bae767735360005e18e27830f94d3df4c79b3
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-20 19:09:16 +0900

    フォームの情報をサーバーサイドに送信

--- app/javascript/channels/card.js
+++ app/javascript/channels/card.js
@@ -26,6 +26,8 @@ const pay = () => {
       document.getElementById("purchase_address_cvc").removeAttribute("name");
       document.getElementById("purchase_address_exp_month").removeAttribute("name");
       document.getElementById("purchase_address_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
 };
