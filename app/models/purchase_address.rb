bfd5e97fe6ba9de68b361c1c505da185ab199393
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-23 12:35:33 +0900

    バリデーションを実装し、テストコードを実装

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :city, :house_number, :phone_number, :building_name, :prefecture_id
+  attr_accessor :user_id, :item_id, :postal_code, :city, :house_number, :phone_number, :building_name, :prefecture_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)", allow_blank: true }
@@ -10,6 +10,7 @@ class PurchaseAddress
     validates :phone_number, format: { with: /\A0[0-9]+\z/, message: 'number is invalid. Include half-width numbers' , allow_blank: true } 
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
