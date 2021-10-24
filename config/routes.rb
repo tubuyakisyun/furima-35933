ddd5cb55d421e9a70f00ba173aa83a27d16f65e8
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-24 18:29:06 +0900

    resources :purchases, only: [:index,:new,:create]のnewアクションは不要なため削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items do
-    resources :purchases, only: [:index,:new,:create]
+    resources :purchases, only: [:index,:create]
   end 
   root to: 'items#index'
 end
