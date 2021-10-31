1ad5fc5cd26a8aefd6377db38fb995e8eb152f92
tubuyakisyun <lcc3344lwm@gmail.com>
2021-10-31 20:04:06 +0900

    https://pikawaka.com/rails/ransack#検索をするためのアクションを追加しよう

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items do
-    resources :purchases, only: [:index,:create]
+    resources :purchases, only: [:index,:create] do
+      collection do
+        get 'search'
+      end
+    end
   end 
   root to: 'items#index'
 end
